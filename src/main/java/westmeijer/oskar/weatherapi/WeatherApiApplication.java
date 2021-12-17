ae978dfcd68e7454dfdb617f717efa23011be111
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2021-12-17 18:48:07 +0100

    add weather code

---
+++ src/main/java/westmeijer/oskar/weatherapi/WeatherApiApplication.java
@@ -0,0 +1,14 @@
+package westmeijer.oskar.weatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApiApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/westmeijer/oskar/weatherapi/WeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package westmeijer.oskar.weatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WeatherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
