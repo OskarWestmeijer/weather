a1a4f8ad40f7bf9d001903ceaf07877544bb0551
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2021-12-22 20:53:14 +0100

    fix webflux config

--- src/main/java/westmeijer/oskar/weatherapi/configuration/WebFluxConfig.java
+++ src/main/java/westmeijer/oskar/weatherapi/configuration/WebFluxConfig.java
@@ -6,13 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@EnableWebFlux
-public class WebFluxConfig implements WebFluxConfigurer {
+public class WebFluxConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(WebFluxConfig.class);
 
