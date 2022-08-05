131be6434439e0c237c1d3d16d9faaa012a679cf
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2022-08-05 17:02:35 +0200

    add profile logging to app

--- src/main/java/westmeijer/oskar/weatherapi/WeatherApiApplication.java
+++ src/main/java/westmeijer/oskar/weatherapi/WeatherApiApplication.java
@@ -1,13 +1,33 @@
 package westmeijer.oskar.weatherapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class WeatherApiApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(WeatherApiApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(WeatherApiApplication.class, args);
+    }
+
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        final Environment env = event.getApplicationContext()
+                .getEnvironment();
+
+        logger.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+        logger.info("Default profiles: {}", Arrays.toString(env.getDefaultProfiles()));
+
+
     }
 
 
