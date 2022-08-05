55709b08bd218d6a8ae4103faa8fe297519905f6
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2022-08-05 17:02:53 +0200

    add doc for schedulung config

--- src/main/java/westmeijer/oskar/weatherapi/configuration/SchedulingConfiguration.java
+++ src/main/java/westmeijer/oskar/weatherapi/configuration/SchedulingConfiguration.java
@@ -4,13 +4,16 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * The scheduling property is disable for Integration Tests with Testcontainers.
+ * Have a look at the SystemTest class in the test package.
+ */
 @EnableScheduling
 @ConditionalOnProperty(
         value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true
 )
 @Configuration
 public class SchedulingConfiguration {
-
 
 
 }
