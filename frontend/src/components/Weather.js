28517a95786975882093bab0a2153aeca1f42456
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2022-08-15 20:12:13 +0200

    add weather request to ui

--- frontend/src/components/Weather.js
+++ frontend/src/components/Weather.js
@@ -1,17 +1,65 @@
 import React from "react";
+const axios = require('axios');
 
-export default function Weather() {
-    return (
-        <div className="container-sm mt-3">
-            <div className="row">
-            <div className="col-md-2 col-sm-12"/>
-                <div className="col-md-8 col-sm-12">
-                    <h2>Weather data for Luebeck, Germany</h2>
-                    <p>Display table with data.</p>
-                </div>
-                <div className="col-md1 col-sm-12">
+
+
+export default class Weather extends React.Component {
+
+    state = {
+        weatherItems: []
+    }
+
+    componentDidMount() {
+
+        var jsonRequest = {
+            'Content-Type': 'application/json;charset=UTF-8',
+        }
+
+        axios.get(`http://localhost:8080/api/weather/23552`,
+            {
+                mode: 'cors',
+                headers: jsonRequest
+            })
+            .then(res => {
+                this.setState({ weatherItems: res.data.weatherData });
+            })
+
+    }
+
+
+    render() {
+        return (
+            <div className="container-sm mt-3">
+                <div className="row">
+                    <div className="col-md-2 col-sm-12" />
+                    <div className="col-md-8 col-sm-12">
+                        <h2 className="mt-3">Weather data for Luebeck, Germany</h2>
+                        <table className="mt-4 table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">temperature</th>
+                                    <th scope="col">humidity</th>
+                                    <th scope="col">wind_speed</th>
+                                    <th scope="col">recorded_at</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.weatherItems.map(item =>
+                                        <tr>
+                                            <td key={item.temperature}>{item.temperature}</td>
+                                            <td key={item.humidity}>{item.humidity}</td>
+                                            <td key={item.windSpeed}>{item.windSpeed}</td>
+                                            <td key={item.recordedAt}>{item.recordedAt}</td>
+                                        </tr>
+                                    )}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="col-md1 col-sm-12">
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
--- src/main/java/westmeijer/oskar/weatherapi/web/WeatherController.java
+++ src/main/java/westmeijer/oskar/weatherapi/web/WeatherController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import westmeijer.oskar.weatherapi.business.WeatherService;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 public class WeatherController {
 
     private static final Logger logger = LoggerFactory.getLogger(WeatherController.class);
