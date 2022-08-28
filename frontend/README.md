0e9b7e2c6ae96ee4a112cf89089c704ae7e390c0
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2022-08-28 13:33:02 +0200

    update url pathes in ui, bump to 1.7.1

--- frontend/README.md
+++ frontend/README.md
@@ -5,6 +5,6 @@
 ```
 npm run build
 
-docker build -t oskarwestmeijer/weather-ui:1.7.0 .
-docker push oskarwestmeijer/weather-ui:1.7.0
+docker build -t oskarwestmeijer/weather-ui:1.7.1 .
+docker push oskarwestmeijer/weather-ui:1.7.1
 ```
--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: "https://oskar-westmeijer.com/api/",
+    baseURL: "https://oskar-westmeijer.com/api/v1/",
     mode: 'cors',
     headers: {
         "Content-type": "application/json;charset=UTF-8"
