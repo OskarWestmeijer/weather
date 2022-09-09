0252a411407796f0fa8d099a7530e327e287e47d
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2022-09-09 17:00:56 +0200

    fix test

--- src/test/java/westmeijer/oskar/weatherapi/WeatherApiApplicationTests.java
+++ src/test/java/westmeijer/oskar/weatherapi/WeatherApiApplicationTests.java
@@ -1,13 +1,28 @@
 package westmeijer.oskar.weatherapi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import westmeijer.oskar.weatherapi.repository.database.LocationRepository;
+import westmeijer.oskar.weatherapi.service.WeatherImportJob;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class WeatherApiApplicationTests {
+
+    @Mock
+    private LocationRepository locationRepository;
+
+    @InjectMocks
+    private WeatherImportJob weatherImportJob;
 
     @Test
     void contextLoads() {
+
+
     }
 
 }
