ec27140ff31a9c6846f0d50dd4f3b6ab861b31ac
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-06-12 23:18:50 +0200

    feat: add private network

--- terraform/database.tf
+++ terraform/database.tf
@@ -3,6 +3,20 @@ resource "google_compute_network" "peering_network" {
   auto_create_subnetworks = "false"
 }
 
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.peering_network.id
+}
+
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.peering_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
 resource "google_sql_database" "weather_database" {
   name     = "weather-database"
   instance = google_sql_database_instance.weather_database.name
@@ -12,6 +26,9 @@ resource "google_sql_database_instance" "weather_database" {
   name             = "weather-database"
   region           = var.project_region
   database_version = "POSTGRES_15"
+
+  depends_on = [google_service_networking_connection.default]
+
   settings {
     tier      = "db-f1-micro"
     disk_size = 10
