9afec4e5fc36c7699b9e4223d376529651fa6126
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-06-12 23:07:53 +0200

    feat: add private network

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,3 +1,8 @@
+resource "google_compute_network" "peering_network" {
+  name                    = "private-network"
+  auto_create_subnetworks = "false"
+}
+
 resource "google_sql_database" "weather_database" {
   name     = "weather-database"
   instance = google_sql_database_instance.weather_database.name
@@ -12,6 +17,7 @@ resource "google_sql_database_instance" "weather_database" {
     disk_size = 10
     ip_configuration {
       ipv4_enabled = false
+      private_network = google_compute_network.peering_network.id
     }
   }
 
