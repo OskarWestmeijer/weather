2b0ab0b9a113508388c3f5748b22f80aac8741b6
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-07-19 21:53:07 +0200

    feat: query from actual schema

--- src/main/java/westmeijer/oskar/weatherapi/repository/WeatherRepository.java
+++ src/main/java/westmeijer/oskar/weatherapi/repository/WeatherRepository.java
@@ -11,13 +11,13 @@ import java.util.UUID;
 
 public interface WeatherRepository extends JpaRepository<Weather, UUID> {
 
-    @Query(value = "SELECT * FROM weather WHERE recorded_at BETWEEN NOW() - INTERVAL '24 HOURS' AND NOW() AND local_zip_code = :local_zip_code ORDER BY recorded_at DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM weather.weather WHERE recorded_at BETWEEN NOW() - INTERVAL '24 HOURS' AND NOW() AND local_zip_code = :local_zip_code ORDER BY recorded_at DESC", nativeQuery = true)
     List<Weather> getLatestEntries(@Param("local_zip_code") String localZipCode);
 
-    @Query(value = "SELECT * FROM weather WHERE recorded_at BETWEEN NOW() - INTERVAL '3 DAYS' AND NOW() AND local_zip_code = :local_zip_code ORDER BY recorded_at DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM weather.weather WHERE recorded_at BETWEEN NOW() - INTERVAL '3 DAYS' AND NOW() AND local_zip_code = :local_zip_code ORDER BY recorded_at DESC", nativeQuery = true)
     List<Weather> getLastThreeDays(@Param("local_zip_code") String localZipCode);
 
-    @Query(value = "SELECT * FROM weather WHERE recorded_at >= :start_date AND recorded_at <= :end_date AND local_zip_code = :local_zip_code ORDER BY recorded_at DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM weather.weather WHERE recorded_at >= :start_date AND recorded_at <= :end_date AND local_zip_code = :local_zip_code ORDER BY recorded_at DESC", nativeQuery = true)
     List<Weather> getSpecificDay(@Param("local_zip_code") String localZipCode, @Param("start_date") Instant start, @Param("end_date") Instant end);
 
 }
