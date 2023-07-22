673a1a6ec4f8daac51c64522cff8b515c1d32b58
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-07-22 21:51:54 +0200

    fix: its

--- src/main/java/westmeijer/oskar/weatherapi/controller/LocationController.java
+++ src/main/java/westmeijer/oskar/weatherapi/controller/LocationController.java
@@ -2,6 +2,7 @@ package westmeijer.oskar.weatherapi.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
