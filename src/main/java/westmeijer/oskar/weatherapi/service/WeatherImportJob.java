5850f8e51f79ac617d89005f88e0847b9dbce5db
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-07-24 14:16:46 +0200

    cleanup import job

--- src/main/java/westmeijer/oskar/weatherapi/service/WeatherImportJob.java
+++ src/main/java/westmeijer/oskar/weatherapi/service/WeatherImportJob.java
@@ -4,6 +4,8 @@ import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,49 +17,46 @@ import westmeijer.oskar.weatherapi.openweatherapi.OpenWeatherApiRequestException
 import westmeijer.oskar.weatherapi.repository.jpa.LocationJpaRepository;
 import westmeijer.oskar.weatherapi.repository.jpa.WeatherJpaRepository;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class WeatherImportJob {
 
-  private static final Logger logger = LoggerFactory.getLogger(WeatherImportJob.class);
-
-  private final Counter importError;
-  private final Counter importExecution;
+  private final MeterRegistry meterRegistry;
 
   private final WeatherJpaRepository weatherJpaRepository;
+
+  private final LocationService locationService;
+
   private final LocationJpaRepository locationJpaRepository;
 
   private final OpenWeatherApiClient openWeatherApiClient;
-
-  public WeatherImportJob(MeterRegistry meterRegistry, WeatherJpaRepository weatherJpaRepository,
-      LocationJpaRepository locationJpaRepository, OpenWeatherApiClient openWeatherApiClient) {
-    this.importError = meterRegistry.counter("job", "import", "error");
-    this.importExecution = meterRegistry.counter("job", "import", "execution");
-    this.weatherJpaRepository = weatherJpaRepository;
-    this.locationJpaRepository = locationJpaRepository;
-    this.openWeatherApiClient = openWeatherApiClient;
-  }
 
 
   @Scheduled(fixedDelay = 60000)
   public void refreshWeather() {
     try {
-      importExecution.increment();
+      meterRegistry.counter("job", "import", "execution").increment();
+
+      Instant importStart = Instant.now().truncatedTo(ChronoUnit.MILLIS);
       LocationEntity locationEntity = locationJpaRepository.findFirstByOrderByLastImportAtAsc();
-      Instant now = Instant.now().truncatedTo(ChronoUnit.MILLIS);
-      locationEntity.setModifiedAt(now);
-      locationEntity.setLastImportAt(now);
-      logger.info("Request for location: {}", locationEntity);
-      WeatherEntity weatherEntity = openWeatherApiClient.requestWeather(locationEntity);
-      WeatherEntity savedWeatherEntity = weatherJpaRepository.saveAndFlush(weatherEntity);
-      logger.info("Saved weather entity: {}", savedWeatherEntity);
+      locationEntity.setModifiedAt(importStart);
+      locationEntity.setLastImportAt(importStart);
+      log.info("Import weather for location: {}", locationEntity);
+
+      WeatherEntity importedWeather = openWeatherApiClient.requestWeather(locationEntity);
+      WeatherEntity savedWeatherEntity = weatherJpaRepository.saveAndFlush(importedWeather);
+      log.info("Imported weather: {}", savedWeatherEntity);
       locationJpaRepository.saveAndFlush(locationEntity);
+
     } catch (OpenWeatherApiRequestException requestException) {
-      logger.error("OpenWeatherApi request failed!", requestException);
-      importError.increment();
+      log.error("OpenWeatherApi request failed!", requestException);
+      meterRegistry.counter("job", "import", "error").increment();
     } catch (Exception generalException) {
-      logger.error("Exception during Import job.", generalException);
-      importError.increment();
+      log.error("Exception during Import job.", generalException);
+      meterRegistry.counter("job", "import", "error").increment();
     }
+
   }
 
 }
