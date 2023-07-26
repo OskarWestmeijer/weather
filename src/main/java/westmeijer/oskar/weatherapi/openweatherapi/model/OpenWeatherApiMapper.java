dccb2579338f4f19d87e3f70dcd4809b5d269c47
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-07-26 17:03:55 +0200

    feat: rework apiclient tests

--- src/main/java/westmeijer/oskar/weatherapi/openweatherapi/model/OpenWeatherApiMapper.java
+++ src/main/java/westmeijer/oskar/weatherapi/openweatherapi/model/OpenWeatherApiMapper.java
@@ -4,11 +4,13 @@ import com.google.common.base.Preconditions;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
+import org.springframework.stereotype.Component;
 import westmeijer.oskar.weatherapi.service.model.Weather;
 
+@Component
 public class OpenWeatherApiMapper {
 
-  public static Weather map(OpenWeatherApiResponse response, String localZipCode) {
+  public Weather map(OpenWeatherApiResponse response, String localZipCode) {
     Preconditions.checkNotNull(response);
     double temperature = response.main().temperature();
     int humidity = response.main().humidity();
