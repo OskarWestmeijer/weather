115dad172f56bd994befe6fc0fff0698ef554c0d
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-07-29 14:51:51 +0200

    fix: set production url

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
-    //baseURL: "https://api.oskar-westmeijer.com",
-    baseURL: "http://localhost:8080",
+    baseURL: "https://api.oskar-westmeijer.com",
+    // baseURL: "http://localhost:8080",
     mode: 'cors',
     headers: {
         "Content-type": "application/json;charset=UTF-8"
