705115bed9c9c78d328d60a4109001e4d4943943
Oskar Westmeijer <26967048+OskarWestmeijer@users.noreply.github.com>
2023-10-03 20:29:22 +0200

    feat: add frontend test (#125)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM eclipse-temurin:17.0.7_7-jre-alpine
+FROM eclipse-temurin:17.0.8.1_1-jre-alpine
 
 COPY target/app.jar /app.jar
 ENV JAVA_OPTS="-Xms750m -Xmx750m"
---
+++ frontend/src/components/documentation/Documentation.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import Documentation from "./Documentation";
+
+it("renders documentation component", () => {
+    render(<Documentation />);
+});
