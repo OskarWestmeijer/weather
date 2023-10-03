45091a44f5b36c3a5b30026363389461de385b61
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-10-03 20:51:04 +0200

    add more tests

--- frontend/src/components/export/Export.js
+++ frontend/src/components/export/Export.js
@@ -1,6 +1,5 @@
 import { React, useState, useEffect } from "react";
-import apiClient from "../../http-common";
-import { weatherApiBaseURL } from "../../http-common";
+import { apiClient, weatherApiBaseURL } from "../../http-common";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
