3af6e9e9e3ffee010b61f1eb25595911b8dbb3c0
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-10-15 19:47:21 +0200

    rework

---
+++ frontend_angular/weather-ui/src/app/overview/overview.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OverviewComponent } from './overview/overview.component';
+
+
+
+@NgModule({
+  declarations: [
+    OverviewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OverviewModule { }
