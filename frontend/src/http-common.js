d226ee459befb5979a8194728ee2b595faac0b86
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-10-15 12:57:09 +0200

    fix: correct api lookup

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const weatherApiBaseURL = process.env.REACT_APP_DEPLOYMENT_ENV === 'production' ? 'https://api.oskar-westmeijer.com' : 'http://localhost:8080';
+const weatherApiBaseURL = process.env.NODE_ENV === 'production' ? 'https://api.oskar-westmeijer.com' : 'http://localhost:8080';
 
 const apiClient = axios.create({
     baseURL: weatherApiBaseURL,
