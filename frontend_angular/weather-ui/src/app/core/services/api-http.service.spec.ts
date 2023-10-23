1c30e53b9444affae128db29deee58a9528bd21c
Oskar Westmeijer <26967048+OskarWestmeijer@users.noreply.github.com>
2023-10-23 22:31:31 +0200

    feat: add service unit tests (#145)

---
+++ frontend_angular/weather-ui/src/app/core/services/api-http.service.spec.ts
@@ -0,0 +1,58 @@
+import { of } from 'rxjs';
+import { Constants } from "src/app/config/constants";
+import { LocationsResponse } from "src/app/model/locations-response";
+import { ApiHttpService } from "./api-http.service";
+import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+describe('ApiHttpService tests', () => {
+    let apiHttpService: ApiHttpService;
+    let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+    beforeEach(() => {
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+        apiHttpService = new ApiHttpService(httpClientSpy, new Constants());
+    });
+
+    it('getLocations empty array', () => {
+        const expectedLocationResponse: LocationsResponse = { locations: [] };
+
+        httpClientSpy.get.and.returnValue(of(expectedLocationResponse));
+
+        apiHttpService.getLocations().subscribe({
+            next: actualResponse => {
+                expect(actualResponse)
+                    .withContext('expected response')
+                    .toEqual(expectedLocationResponse);
+                    expect(actualResponse.locations.length)
+                    .withContext('has no location entry')
+                    .toBe(0)
+            }
+        });
+        expect(httpClientSpy.get.calls.count())
+            .withContext('one call')
+            .toBe(1);
+    });
+
+    it('getLocations with one entry', () => {
+        const expectedLocationResponse: LocationsResponse = { locations: [{ cityName: 'Helsinki', country: 'Finland', countryCode: 'FIN', localZipCode: '425534', lastImportAt: 'today', locationCode: '234235', uuid: '3254' }] };
+
+        httpClientSpy.get.and.returnValue(of(expectedLocationResponse));
+
+        apiHttpService.getLocations().subscribe({
+            next: actualResponse => {
+                expect(actualResponse)
+                    .withContext('expected response')
+                    .toEqual(expectedLocationResponse);
+                expect(actualResponse.locations.length)
+                    .withContext('has one location entry')
+                    .toBe(1)
+            }
+        });
+        expect(httpClientSpy.get.calls.count())
+            .withContext('one call')
+            .toBe(1);
+    });
+
+});
--- frontend_angular/weather-ui/src/app/overview/overview/overview.component.spec.ts
+++ frontend_angular/weather-ui/src/app/overview/overview/overview.component.spec.ts
@@ -2,27 +2,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApiHttpService } from 'src/app/core/services/api-http.service';
 import { OverviewComponent } from './overview.component';
 import { waitForAsync } from '@angular/core/testing';
-import { of } from 'rxjs';
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
   let fixture: ComponentFixture<OverviewComponent>;
   const mockApiHttpService = jasmine.createSpyObj("ApiHttpService", ["getLocations"]);
 
-  // TODO: https://ng-mocks.sudo.eu/extra/mock-observables/
-
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [OverviewComponent],
-      providers: [MockProvider(ApiHttpService)]
+      providers: [{ provide: ApiHttpService, useValue: mockApiHttpService }]
     }).compileComponents();
+
     fixture = TestBed.createComponent(OverviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
   it('should create', () => {
-    mockApiHttpService.getLocations.and.returnValue(of({}));
+    mockApiHttpService.getLocations.and.returnValue('');
+    expect(mockApiHttpService.getLocations).toHaveBeenCalled();
 
     expect(component).toBeTruthy();
   });
