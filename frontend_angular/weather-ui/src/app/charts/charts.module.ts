d96cc8ee72735af789c5d36e157aa88d4a930c1c
Oskar Westmeijer <26967048+OskarWestmeijer@users.noreply.github.com>
2023-10-26 22:08:06 +0200

    add two way binding (#153)

--- frontend_angular/weather-ui/src/app/charts/charts.module.ts
+++ frontend_angular/weather-ui/src/app/charts/charts.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartsComponent } from './charts/charts.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [ChartsComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     exports: [ChartsComponent]
 })
 export class ChartsModule {}
