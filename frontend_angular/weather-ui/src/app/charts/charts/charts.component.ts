22e91e38d80c1edd2a22ab3571beea3d7f5f05b5
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-10-26 21:56:16 +0200

    add two way binding

--- frontend_angular/weather-ui/src/app/charts/charts/charts.component.ts
+++ frontend_angular/weather-ui/src/app/charts/charts/charts.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiHttpService } from 'src/app/core/services/api-http.service';
+import { LocationsResponse } from 'src/app/model/locations-response';
+import { Location } from 'src/app/model/location';
 
 @Component({
     selector: 'app-charts',
     templateUrl: './charts.component.html',
     styleUrls: ['./charts.component.css']
 })
-export class ChartsComponent {}
+export class ChartsComponent implements OnInit {
+    public locationList: Location[] = [];
+    public selectedLocation: string = '';
+
+    constructor(private apiHttpService: ApiHttpService) {}
+
+    ngOnInit() {
+        this.getLocations();
+    }
+
+    public onSelectChange(locationId: string): void {
+        console.log('changed to locationId: ' + locationId);
+    }
+
+    private getLocations(): void {
+        this.apiHttpService
+            .getLocations()
+            .subscribe((locationsResponse: LocationsResponse) => {
+                if (locationsResponse !== undefined) {
+                    this.locationList = locationsResponse.locations;
+                    this.selectedLocation = this.locationList[0]?.uuid;
+                } else {
+                    console.log('Undefined response.');
+                }
+            });
+    }
+}
