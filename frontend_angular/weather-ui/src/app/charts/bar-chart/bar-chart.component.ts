9b30baf70aefcde27ebd3f7babb226cc853874dc
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-10-30 00:01:38 +0100

    add more chart options

--- frontend_angular/weather-ui/src/app/charts/bar-chart/bar-chart.component.ts
+++ frontend_angular/weather-ui/src/app/charts/bar-chart/bar-chart.component.ts
@@ -38,7 +38,9 @@ export class BarChartComponent implements OnInit, OnChanges {
                             backgroundColor: 'orange',
                             borderColor: 'orange',
                             fill: false,
-                            yAxisID: 'temperatureY'
+                            tension: 0.25,
+                            yAxisID: 'temperatureY',
+                            order: 1
                         },
                         {
                             type: 'bar',
@@ -46,15 +48,8 @@ export class BarChartComponent implements OnInit, OnChanges {
                             data: this.dataMap.get(ChartType.HUMIDITY)?.map((item) => item.data),
                             borderColor: 'rgb(255, 99, 132)',
                             backgroundColor: 'rgba(75,192,192,0.2)',
-                            yAxisID: 'humidityY'
-                        },
-                        {
-                            type: 'bar',
-                            label: ChartType.WIND_SPEED,
-                            data: this.dataMap.get(ChartType.WIND_SPEED)?.map((item) => item.data),
-                            backgroundColor: 'rgba(0,0,0,0.2)',
-                            xAxisID: 'topX',
-                            yAxisID: 'windSpeedY'
+                            yAxisID: 'humidityY',
+                            order: 2
                         }
                     ]
                 },
@@ -62,30 +57,22 @@ export class BarChartComponent implements OnInit, OnChanges {
                     aspectRatio: 2.7,
                     scales: {
                         temperatureY: {
+                            beginAtZero: true,
                             position: 'left'
                         },
                         humidityY: {
                             position: 'right',
                             ticks: {
                                 stepSize: 10,
+                                maxTicksLimit: 100,
                                 callback: function (value, index, ticks) {
                                     return value + '%';
                                 }
+                            },
+                            grid: {
+                                display: false
                             }
-                        },
-                        windSpeedY: {
-                            display: false
-                        },
-                        topX: {
-                            position: 'top',
-                            display: true,
-                            ticks: {
-                                stepSize: 10,
-                                callback: function (value, index, ticks) {
-                                    return index.toString();
-                                }
-                            }
-                        },
+                        }
                     }
                 }
             });
