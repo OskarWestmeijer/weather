22b77f9ac8e048d5719edcb37353a82fd0dda485
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-10-30 17:25:55 +0100

    update shell script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 echo 'Start deploy weather-api script.'
 
-compose_down="docker-compose -f cprod.yml down"
+compose_down="docker compose -f cprod.yml down"
 list_images="docker images"
 rm_latest_api="docker image rm oskarwestmeijer/weather-api:latest"
 rm_latest_ui="docker image rm oskarwestmeijer/weather-ui:latest"
-compose_up="docker-compose -f cprod.yml up -d"
+compose_up="docker compose -f cprod.yml up -d"
 
 $compose_down
 $list_images
