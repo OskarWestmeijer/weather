d8b0658d5022c81c41aa300a3606b7ee2a30b354
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-11-11 00:26:04 +0100

    migrate to tailwind + daisyui

--- frontend/weather-ui/tailwind.config.js
+++ frontend/weather-ui/tailwind.config.js
@@ -4,5 +4,33 @@ module.exports = {
     theme: {
         extend: {}
     },
-    plugins: [require('daisyui')]
+    plugins: [require('daisyui')],
+    daisyui: {
+        themes: [
+            'light',
+            'dark',
+            'cupcake',
+            {
+                customtheme: {
+                    primary: '#529b03',
+
+                    secondary: '#e9e92f',
+
+                    accent: '#f6f9c8',
+
+                    neutral: '#191a3e',
+
+                    'base-100': '#ffffff',
+
+                    info: '#cae2e8',
+
+                    success: '#dff2a1',
+
+                    warning: '#f7e488',
+
+                    error: '#f2b6b5'
+                }
+            }
+        ]
+    }
 };
