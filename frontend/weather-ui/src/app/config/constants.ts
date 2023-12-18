01ceb8837e5c9428ccb6786eb64b73fbb96975db
Oskar Westmeijer <26967048+OskarWestmeijer@users.noreply.github.com>
2023-12-18 19:14:54 +0100

    feat: update charts (#231)

--- frontend/weather-ui/src/app/config/constants.ts
+++ frontend/weather-ui/src/app/config/constants.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
 @Injectable()
 export class Constants {
-    public readonly API_ENDPOINT: string = 'https://api.oskar-westmeijer.com';
+    public readonly API_ENDPOINT: string = environment.apiUrl;
+
     public readonly API_MOCK_ENDPOINT: string = 'https://localhost:8080';
 }
