437cfae40ed022eefb7efeeaf72f00c1b4ace4b0
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-04-20 20:06:26 +0200

    feat: edit linter issues

--- frontend/weather-ui/src/app/details-page/timelapse-chart/timelapse-chart.component.ts
+++ frontend/weather-ui/src/app/details-page/timelapse-chart/timelapse-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import Chart from 'chart.js/auto';
+import { Chart } from 'chart.js/auto';
 import { ChartData } from 'src/app/model/chart-data.model';
 import { ChartType } from 'src/app/model/chart-type.enum';
 
@@ -9,10 +9,10 @@ import { ChartType } from 'src/app/model/chart-type.enum';
     styleUrls: ['./timelapse-chart.component.css']
 })
 export class TimelapseChartComponent implements OnInit, OnChanges {
-    @Input({ required: true }) dataMap?: Map<ChartType, ChartData[]>;
+    @Input({ required: true }) dataMap!: Map<ChartType, ChartData[]>;
     @Input({ required: true }) chartType?: ChartType;
 
-    public chart: any;
+    public chart!: Chart;
     private colorWhite = 'white';
     private colorOrange = 'rgba(255, 159, 64, 1)';
 
@@ -29,55 +29,59 @@ export class TimelapseChartComponent implements OnInit, OnChanges {
     private createChart(): void {
         if (this.dataMap != undefined && this.chartType != undefined) {
             Chart.getChart('BarChart')?.destroy();
-            this.chart = new Chart('BarChart', {
-                data: {
-                    labels: this.dataMap.get(ChartType.TEMPERATURE)?.map((item) => item.recordedAt),
-                    datasets: [
-                        {
-                            type: 'line',
-                            label: ChartType.TEMPERATURE,
-                            data: this.dataMap.get(ChartType.TEMPERATURE)?.map((item) => item.data),
-                            backgroundColor: this.colorOrange,
-                            borderColor: this.colorOrange,
-                            tension: 0.25,
-                        }
-                    ]
+            this.chart = new Chart('BarChart', this.createChartConfig());
+        }
+    }
+
+    private createChartConfig(): any {
+        return {
+            data: {
+                labels: this.dataMap.get(ChartType.TEMPERATURE)?.map((item) => item.recordedAt),
+                datasets: [
+                    {
+                        type: 'line',
+                        label: ChartType.TEMPERATURE,
+                        data: this.dataMap.get(ChartType.TEMPERATURE)?.map((item) => item.data),
+                        backgroundColor: this.colorOrange,
+                        borderColor: this.colorOrange,
+                        tension: 0.25
+                    }
+                ]
+            },
+            options: {
+                datasets: {
+                    line: {
+                        borderWidth: 5
+                    }
                 },
-                options: {
-                    datasets: {
-                        line: {
-                            borderWidth: 5
-                        }
-                    },
-                    aspectRatio: 2.7,
-                    scales: {
-                        x: {
-                            border: {
-                                color: this.colorWhite
-                            },
-                            ticks: {
-                                color: this.colorWhite
-                            }
+                aspectRatio: 2.7,
+                scales: {
+                    x: {
+                        border: {
+                            color: this.colorWhite
                         },
-                        y: {
-                            beginAtZero: true,
-                            ticks: {
-                                color: this.colorWhite
-                            },
-                            border: {
-                                color: this.colorWhite
-                            },
+                        ticks: {
+                            color: this.colorWhite
                         }
                     },
-                    plugins: {
-                        legend: {
-                            labels: {
-                                color: this.colorWhite,
-                            }
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            color: this.colorWhite
+                        },
+                        border: {
+                            color: this.colorWhite
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        labels: {
+                            color: this.colorWhite
                         }
                     }
                 }
-            });
-        }
+            }
+        };
     }
 }
