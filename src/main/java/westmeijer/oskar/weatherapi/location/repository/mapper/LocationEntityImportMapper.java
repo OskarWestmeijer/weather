86ef820c57f24ecaffc68e5394622275cc995b96
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-04-21 22:00:53 +0200

    feat: tweak saving mechanic

--- src/main/java/westmeijer/oskar/weatherapi/location/repository/mapper/LocationEntityImportMapper.java
+++ src/main/java/westmeijer/oskar/weatherapi/location/repository/mapper/LocationEntityImportMapper.java
@@ -1,14 +1,18 @@
 package westmeijer.oskar.weatherapi.location.repository.mapper;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Builder;
 import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 import westmeijer.oskar.weatherapi.location.repository.model.LocationEntity;
 import westmeijer.oskar.weatherapi.location.service.model.Location;
-import westmeijer.oskar.weatherapi.weather.repository.model.WeatherEntity;
-import westmeijer.oskar.weatherapi.weather.service.model.Weather;
 
 @Mapper(componentModel = "spring", collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED, builder = @Builder(disableBuilder = true))
 public interface LocationEntityImportMapper {
@@ -18,4 +22,9 @@ public interface LocationEntityImportMapper {
   @Mapping(target = "modifiedAt", expression = "java(java.time.Instant.now().truncatedTo(java.time.temporal.ChronoUnit.MICROS))")
   LocationEntity mapToLocationEntity(Location location);
 
+  @AfterMapping
+  default void setModifiedAt(@MappingTarget LocationEntity locationEntity, Location location) {
+    checkArgument(locationEntity.getWeather().size() == 1, "Only modifying imported weather");
+    locationEntity.getWeather().getFirst().setModifiedAt(Instant.now().truncatedTo(ChronoUnit.MICROS));
+  }
 }
