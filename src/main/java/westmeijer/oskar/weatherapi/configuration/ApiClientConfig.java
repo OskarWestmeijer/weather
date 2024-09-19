b8c3503d2b4fcaae8a3c034455019d2a8fe56111
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-09-19 22:38:51 +0200

    fix: add timeout configuration to api client

--- src/main/java/westmeijer/oskar/weatherapi/configuration/ApiClientConfig.java
+++ src/main/java/westmeijer/oskar/weatherapi/configuration/ApiClientConfig.java
@@ -1,9 +1,13 @@
 package westmeijer.oskar.weatherapi.configuration;
 
+import io.netty.channel.ChannelOption;
+import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import reactor.netty.http.client.HttpClient;
 import westmeijer.oskar.weatherapi.openapi.client.ApiClient;
 import westmeijer.oskar.weatherapi.openapi.client.api.GeneratedOpenWeatherApi;
 
@@ -19,7 +23,17 @@ public class ApiClientConfig {
 
   @Bean
   public GeneratedOpenWeatherApi generatedOpenWeatherApi() {
-    ApiClient apiClient = new ApiClient();
+    var timeout = Duration.ofSeconds(10);
+
+    var webClient = ApiClient.buildWebClientBuilder()
+        .clientConnector(new ReactorClientHttpConnector(
+            HttpClient.create()
+                .responseTimeout(timeout)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) timeout.toMillis())
+        ))
+        .build();
+
+    ApiClient apiClient = new ApiClient(webClient);
     apiClient.setBasePath(baseUrl);
     return new GeneratedOpenWeatherApi(apiClient);
   }
