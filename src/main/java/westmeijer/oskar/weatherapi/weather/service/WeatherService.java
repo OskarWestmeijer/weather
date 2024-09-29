af99c52fabc48c10823bb55a00bea539bb5a4019
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-09-29 18:46:40 +0200

    feat: add tests

--- src/main/java/westmeijer/oskar/weatherapi/weather/service/WeatherService.java
+++ src/main/java/westmeijer/oskar/weatherapi/weather/service/WeatherService.java
@@ -3,7 +3,6 @@ package westmeijer.oskar.weatherapi.weather.service;
 import static java.util.Objects.requireNonNull;
 
 import java.time.Instant;
-import java.util.Comparator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,15 +28,6 @@ public class WeatherService {
     requireNonNull(locationId, "locationId is required");
     requireNonNull(from, "from is required");
     return weatherRepository.getTotalCount(locationId, from);
-  }
-
-  public List<Weather> getLast24h(Integer locationId) {
-    requireNonNull(locationId, "locationId is required");
-    List<Weather> weatherList = weatherRepository.getLast24h(locationId);
-
-    return weatherList.stream()
-        .sorted(Comparator.comparing(Weather::recordedAt).reversed())
-        .toList();
   }
 
 }
